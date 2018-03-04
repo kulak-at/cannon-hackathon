5056891b213c7f72a0489fb65f1d3bc4ff9a4715
Natalia Mecner <natalia.mecner@gmail.com>
2018-03-04 14:13:02 +0100

    Added Fourier Transformate on sound and Websocket

--- measure_frequency.py
+++ measure_frequency.py
@@ -1,38 +1,60 @@
 import pyaudio
+import struct
 import numpy as np
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
+import websockets
+import asyncio
 
 CHUNK = 2**11
 RATE = 44100
+nFFT = 6
+BUF_SIZE = 4 * nFFT
+SAMPLE_SIZE = 2
+CHANNELS = 2
+MAX_y = 2.0 ** (SAMPLE_SIZE * 8 - 1)
 
-p=pyaudio.PyAudio()
-stream=p.open(format=pyaudio.paInt16,channels=2,rate=RATE,input=True,
+p = pyaudio.PyAudio()
+streamGlobal=p.open(format=pyaudio.paInt16,channels=2,rate=RATE,input=True,
               frames_per_buffer=CHUNK)
 
 
+frequency_threshold = [
+    {"start": 60, "end": 250}, {"start": 250, "end": 500}, {"start": 500, "end": 2000},
+    {"start": 2000, "end": 4000}, {"start": 4000, "end": 6000}, {"start": 6000, "end": 20000}]
 
-def animate(i, line, stream, wf, MAX_y):
 
-    N = max(stream.get_read_available() / nFFT, 1) * nFFT
-    data = stream.read(N)
-
-    y = np.array(struct.unpack("%dh" % (N * CHANNELS), data)) / MAX_y
+def animate(stream, MAX_y):
+    n = int(max(stream.get_read_available() / nFFT, 1) * nFFT)
+    data = stream.read(n)
+    y = np.array(struct.unpack("%dh" % (n * CHANNELS), data)) / MAX_y
     y_R = y[1::2]
+    Y_R = abs(np.fft.fft(y_R, nFFT))
 
-    Y_R = np.fft.fft(y_R, nFFT)
+    print(Y_R / 6)
+    return
+    # fft = Y_R[:int(len(Y_R)/2)]
+    # print(fft)
+    freq_initial = np.fft.fftfreq(CHUNK, 1.0/RATE)
+    freq = freq_initial[:int(len(freq_initial) / 2)]
 
 
-    line.set_ydata(Y_R)
-    return line,
+    print(np.mean(freq[np.where(freq < 2000)]))
 
+
+async def hello(websocket, path):
+    greeting = "Hello socket, my old friend"
+    print(greeting)
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
 
 for i in range(int(10*44100/1024)): #go for a few seconds
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    peak=np.average(np.abs(data))*2
-    bars=int(50*peak/2**16)
-    print("%04d %05d %s"%(i,peak,bars))
+    animate(streamGlobal, MAX_y)
 
-stream.stop_stream()
-stream.close()
+
+
+
+streamGlobal.stop_stream()
+streamGlobal.close()
 p.terminate()
