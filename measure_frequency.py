48afaf9b2b9ccbcea23ae27ba5afca00f970b50d
Natalia Mecner <natalia.mecner@gmail.com>
2018-03-04 15:54:20 +0100

    Split spectrum to human-recognizable ranges

--- measure_frequency.py
+++ measure_frequency.py
@@ -4,10 +4,11 @@ import numpy as np
 import websockets
 import asyncio
 import json
+import math
 
 CHUNK = 2**11
 RATE = 44100
-nFFT = 6
+nFFT = 512
 BUF_SIZE = 4 * nFFT
 SAMPLE_SIZE = 2
 CHANNELS = 2
@@ -19,8 +20,12 @@ streamGlobal=p.open(format=pyaudio.paInt16,channels=2,rate=RATE,input=True,
 
 
 frequency_threshold = [
-    {"start": 60, "end": 250}, {"start": 250, "end": 500}, {"start": 500, "end": 2000},
-    {"start": 2000, "end": 4000}, {"start": 4000, "end": 6000}, {"start": 6000, "end": 20000}]
+    {"start": 60, "end": 250},
+    {"start": 250, "end": 500},
+    {"start": 500, "end": 2000},
+    {"start": 2000, "end": 4000},
+    {"start": 4000, "end": 6000},
+    {"start": 6000, "end": 20000}]
 
 
 def animate(stream, MAX_y):
@@ -29,17 +34,12 @@ def animate(stream, MAX_y):
     y = np.array(struct.unpack("%dh" % (n * CHANNELS), data)) / MAX_y
     y_R = y[1::2]
     Y_R = abs(np.fft.fft(y_R, nFFT))
-
-    #print(Y_R / 6)
-    return (Y_R/6).tolist()
-    # # fft = Y_R[:int(len(Y_R)/2)]
-    # # print(fft)
-    # freq_initial = np.fft.fftfreq(CHUNK, 1.0/RATE)
-    # freq = freq_initial[:int(len(freq_initial) / 2)]
-    #
-    #
-    # renp.mean(freq[np.where(freq < 2000)]))
-
+    li = []
+    for n in frequency_threshold:
+        nx = math.floor(n["start"]*nFFT/RATE)
+        ny = math.ceil(n["end"]*nFFT/RATE)
+        li.append(max(Y_R[nx:ny])*(ny-nx))
+    return li
 
 async def hello(websocket, path):
     greeting = "Hello socket, my old friend"
